d62f1b1cf578c7c25d5eceaaf40f0d2db98c8e02
unknown <harshbajani7@gmail.com>
2024-03-18 15:15:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { Analytics } from "@vercel/analytics/react";
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+import Model from "./components/Model";
+import Navbar from "./components/Navbar";
+import Features from "./components/Features";
+import HowItWorks from "./components/HowItWorks";
+import Footer from "./components/Footer";
+import * as Sentry from "@sentry/react";
+
+const App = () => {
+  return (
+    <main className="bg-black">
+      <Analytics>
+        <Navbar />
+        <Hero />
+        <Highlights />
+        <Model />
+        <Features />
+        <HowItWorks />
+        <Footer />
+      </Analytics>
+    </main>
+  );
+};
+
+export default Sentry.withProfiler(App);
---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://28b36b27f48103705476e84b1c950739@o4506930714640384.ingest.us.sentry.io/4506930718375936",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
